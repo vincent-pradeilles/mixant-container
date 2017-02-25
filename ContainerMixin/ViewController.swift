6b95fe0015842b09f85d256edcab36ecd7a5743d
vincent-pradeilles <Vincent@MacBook-Pro-de-Vincent.local>
2017-02-25 15:56:27 +0100

    Implement with a mixin

--- ContainerMixin/ViewController.swift
+++ ContainerMixin/ViewController.swift
@@ -11,41 +11,27 @@ import RxSwift
 import RxCocoa
 
 enum Section: Int {
-    case a
-    case b
-    case c
-    case d
+    case red
+    case blue
+    case green
+    case purple
 }
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, SectionContainer {
 
     @IBOutlet weak var containerSegmentedController: UISegmentedControl!
     @IBOutlet weak var containerView: UIView!
     
     var sectionViewControllers: [UIViewController] = []
-    weak var currentSectionViewController: UIViewController?
+    var currentSection: Section?
     
     lazy var disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        self.bindUI()
         self.createSectionViewControllers()
-        self.transition(toSection: .a)
-    }
-    
-    func bindUI () {
-        containerSegmentedController
-            .rx
-            .selectedSegmentIndex
-            .asDriver()
-            .drive(onNext: { (newSelectedIndex) in
-                if let newSection = Section(rawValue: newSelectedIndex) {
-                    self.transition(toSection: newSection)
-                }
-            })
-            .addDisposableTo(disposeBag)
+        self.hookUpSectionContainer()
     }
     
     func createSectionViewControllers() {
@@ -62,27 +48,5 @@ class ViewController: UIViewController {
         purpleVC.view.backgroundColor = .purple
         
         sectionViewControllers = [redVC, blueVC, greenVC, purpleVC]
-    }
-    
-    func transition(toSection section: Section) {
-        if let newSectionViewController = sectionViewControllers[safe: section.rawValue] {
-            currentSectionViewController?.willMove(toParentViewController: nil)
-            addChildViewController(newSectionViewController)
-            
-            newSectionViewController.view.frame = self.containerView.bounds
-            
-            containerSegmentedController.isUserInteractionEnabled = false
-            
-            UIView.animate(withDuration: 0.25, delay: 0, options: [.transitionCrossDissolve], animations: { 
-                self.currentSectionViewController?.view.removeFromSuperview()
-                self.containerView.addSubview(newSectionViewController.view)
-            }, completion: { (_) in
-                self.currentSectionViewController?.removeFromParentViewController()
-                newSectionViewController.didMove(toParentViewController: self)
-                
-                self.currentSectionViewController = newSectionViewController
-                self.containerSegmentedController.isUserInteractionEnabled = true
-            })
-        }
     }
 }
